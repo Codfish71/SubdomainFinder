d0340af3e078102efd93ee0c4fc12b899fa88c48
ThePcCriminal71 <47088865+ThePcCriminal71@users.noreply.github.com>
2020-11-20 19:41:28 +0530

    Add files via upload

---
+++ subdomainfinder.py
@@ -0,0 +1,16 @@
+import requests
+
+domain = 'www.vulnweb.com'
+
+file = open('subdomnames.txt','r')
+content = file.read()
+subdomains = content.splitlines()
+
+for subdomain in subdomains:
+	url = f'http://{subdomain}.{domain}'
+	try:
+		requests.get(url)
+	except requests.ConnectionError:
+		pass
+	else:
+		print("Discovered subdmain : ",url)
